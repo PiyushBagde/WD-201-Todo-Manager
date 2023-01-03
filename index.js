4bbe651b60cd14e560cec0381fe44302cbc360ac
PiyushBagde <piyushbagde002@gmail.com>
2023-01-03 20:56:30 +0530

    commiting changes

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 /* eslint-disable quotes */
 /* eslint-disable semi */
 const app = require("./app");
+const express = require("express");
+const path = require("path");
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
